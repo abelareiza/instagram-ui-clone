d2743395c55d1f58f67168929a28d7c0214e922c
Abel <abelareiza@gmail.com>
2021-08-30 13:37:42 -0500

    added tailwind to the project, header and stories sections

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './**/*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'gris': '#DBDBDB',
+        'gris2': '#FAFAFA',
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
