6f38ba9532ca6cea7fbe05ea606b1bbce1870201
Abel <abelareiza@gmail.com>
2021-09-08 19:31:45 -0500

    added background and anchor tags corrections

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,23 @@ module.exports = {
         'gris3': '#262626',
         'gris4': '#8E8E8E',
         'azul': '#00376b',
-      }
+      },
+      animation: {
+        tilt: 'tilt 5s infinite linear'
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(1deg)",
+          },
+          "75%": {
+            transform: "rotate(-1deg)",
+          }
+        }
+      },
     }
   },
   variants: {
